a4c537bf1a60c877c2a0352140b372ff633b64e3
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-17 15:49:59 +0700

    Add files via upload

---
+++ backup/restore.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+clear
+echo "Fitur Ini Hanya Dapat Digunakan Sesuai Data VPS Dengan Autoscript Ini"
+echo "Silakan masukkan tautan ke file cadangan data vps Anda."
+echo "Anda dapat memeriksanya di email Anda jika Anda menjalankan backup data vps sebelumnya."
+read -rp "Tautan Berkas: " -e url
+wget -O backup.zip "$url"
+unzip backup.zip
+rm -f backup.zip
+sleep 1
+echo Start Restore
+cd /root/backup
+cp passwd /etc/
+cp group /etc/
+cp shadow /etc/
+cp gshadow /etc/
+cp -r wireguard /etc/
+cp chap-secrets /etc/ppp/
+cp passwd1 /etc/ipsec.d/passwd
+cp ss.conf /etc/shadowsocks-libev/ss.conf
+cp -r akbarstorevpn /var/lib/
+cp -r sstp /home/
+cp -r xray /etc/
+cp -r trojan-go /etc/
+cp -r shadowsocksr /usr/local/
+cp -r public_html /home/vps/
+cp crontab /etc/
+strt
+rm -rf /root/backup
+rm -f backup.zip
+echo Done
